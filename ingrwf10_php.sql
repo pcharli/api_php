fed6d94cb20ed1ca59ea763e5b15c4722b07c190
pierre <pcepegra@gmail.com>
2022-07-07 11:01:27 +0200

    update SQL

--- ingrwf10_php.sql
+++ ingrwf10_php.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 05 juil. 2022 à 16:25
+-- Généré le : jeu. 07 juil. 2022 à 10:59
 -- Version du serveur : 10.4.20-MariaDB
 -- Version de PHP : 8.0.8
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `ingrwf10_php`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `label` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `categories`
+--
+
+INSERT INTO `categories` (`id`, `label`) VALUES
+(1, 'new label'),
+(2, 'jouets'),
+(3, 'outils'),
+(4, 'véhicules'),
+(6, 'new label');
 
 -- --------------------------------------------------------
 
@@ -47,6 +69,18 @@ INSERT INTO `contact` (`id`, `name`, `email`, `message`) VALUES
 (6, 'inconnu', 'dsdsd@ddd.com', 'ssdsds'),
 (7, 'inconnu', 'dsdsd@ddd.com', 'ssdsds'),
 (8, 'pierre charlier', 'charlier.pierre@gmail.com', 'sdsdsdsd');
+
+-- --------------------------------------------------------
+
+--
+-- Doublure de structure pour la vue `marcel`
+-- (Voir ci-dessous la vue réelle)
+--
+CREATE TABLE `marcel` (
+`id_personnes` int(10) unsigned
+,`nom` varchar(120)
+,`prenom` varchar(120)
+);
 
 -- --------------------------------------------------------
 
@@ -92,7 +126,6 @@ INSERT INTO `personnes` (`id_personnes`, `nom`, `prenom`) VALUES
 (2, 'Proust2', 'Marcel2'),
 (6, 'charlier', 'benoit'),
 (19, 'devos', 'françoise'),
-(21, 'brol', 'machin'),
 (22, 'Proust', 'Marcel'),
 (23, 'Proust2', 'Marcel2'),
 (24, 'Proust2', 'Marcel2'),
@@ -105,7 +138,56 @@ INSERT INTO `personnes` (`id_personnes`, `nom`, `prenom`) VALUES
 (31, 'Dupnt', 'Henry'),
 (32, 'dupont', 'marcel'),
 (33, 'proust', 'Emmanuel'),
-(34, 'dupont', 'marcel');
+(34, 'charlier2', 'pierre2'),
+(35, 'test', 'pitou'),
+(36, 'charlier2', 'pierre2'),
+(37, 'charlier2', 'pierre2'),
+(38, 'charlier2', 'pierre2'),
+(39, 'charlier2', 'pierre2');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produits`
+--
+
+CREATE TABLE `produits` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `label` varchar(250) NOT NULL,
+  `id_categories` int(11) DEFAULT NULL,
+  `prix` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `produits`
+--
+
+INSERT INTO `produits` (`id`, `label`, `id_categories`, `prix`) VALUES
+(1, 'pommes', 1, NULL),
+(3, 'new label', NULL, 45),
+(4, 'poires', 1, NULL),
+(5, 'voiture', 4, NULL),
+(6, 'poupée', 2, NULL),
+(7, 'new label2', NULL, 45.3),
+(8, 'new label2', NULL, 45.3),
+(9, 'new label2', NULL, 45.3),
+(10, 'new label2', NULL, 45.3),
+(11, 'new label2', NULL, 45.3);
+
+-- --------------------------------------------------------
+
+--
+-- Doublure de structure pour la vue `produits_categories`
+-- (Voir ci-dessous la vue réelle)
+--
+CREATE TABLE `produits_categories` (
+`id` int(10) unsigned
+,`label` varchar(250)
+,`id_categories` int(11)
+,`prix` float
+,`id_cat` int(11)
+,`cat_label` varchar(250)
+);
 
 -- --------------------------------------------------------
 
@@ -126,9 +208,33 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id_users`, `login`, `password`) VALUES
 (1, 'pierre', 'pass');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la vue `marcel`
+--
+DROP TABLE IF EXISTS `marcel`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `marcel`  AS SELECT `personnes`.`id_personnes` AS `id_personnes`, `personnes`.`nom` AS `nom`, `personnes`.`prenom` AS `prenom` FROM `personnes` WHERE `personnes`.`prenom` like '%marcel%' ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la vue `produits_categories`
+--
+DROP TABLE IF EXISTS `produits_categories`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `produits_categories`  AS SELECT `produits`.`id` AS `id`, `produits`.`label` AS `label`, `produits`.`id_categories` AS `id_categories`, `produits`.`prix` AS `prix`, `categories`.`id` AS `id_cat`, `categories`.`label` AS `cat_label` FROM (`produits` left join `categories` on(`produits`.`id_categories` = `categories`.`id`)) ;
+
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `contact`
@@ -149,6 +255,13 @@ ALTER TABLE `personnes`
   ADD PRIMARY KEY (`id_personnes`);
 
 --
+-- Index pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_categories` (`id_categories`);
+
+--
 -- Index pour la table `users`
 --
 ALTER TABLE `users`
@@ -157,6 +270,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `contact`
@@ -174,13 +293,29 @@ ALTER TABLE `news`
 -- AUTO_INCREMENT pour la table `personnes`
 --
 ALTER TABLE `personnes`
-  MODIFY `id_personnes` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id_personnes` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT pour la table `produits`
+--
+ALTER TABLE `produits`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
   MODIFY `id_users` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD CONSTRAINT `produits_ibfk_1` FOREIGN KEY (`id_categories`) REFERENCES `categories` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
